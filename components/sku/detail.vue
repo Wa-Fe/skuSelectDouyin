eff7bfc7bceb16891078ca89669cba082fdaece0
Wa_Fe <www.520x.com@gmail.com>
2022-09-08 09:44:00 +0800

    feat:first

---
+++ components/sku/detail.vue
@@ -0,0 +1,82 @@
+<template>
+	<view class="skudetail">
+		<view class="title">
+			<text>{{group.name}}</text>
+		</view>
+		<view class="items">
+			<view class="item" v-for="item in group.spec_items" :key="item.id">
+				<view class="itemd" :class="[{'primary':selectedSpecs.findIndex((i) => i.id === item.id) > -1},
+				{'disadleS':unSelectSpecs.includes(item.id)}]"
+					@click="clickSpec(item)">
+					{{item.name}}
+				</view>
+
+			</view>
+		</view>
+	</view>
+</template>
+<script>
+	export default {
+		props: {
+			group: {
+				type: Object,
+				default: () => {}
+			},
+			selectedSpecs: {
+				type: Array,
+				default: () => []
+			},
+			unSelectSpecs:{
+				type: Array,
+				default: () => []
+			}
+		},
+		data() {
+			return {}
+		},
+		onLoad() {},
+		methods: {
+			clickSpec(item) {
+				if(this.unSelectSpecs.includes(item.id)) return
+				this.$emit('change', item, this.group)
+			}
+		}
+	}
+</script>
+<style lang="scss" scoped>
+	.skudetail {
+		.title {
+			margin-top: 15rpx;
+			margin-bottom: 20rpx;
+		}
+
+		.items {
+			display: flex;
+			flex-wrap: wrap;
+
+			.itemd {
+				display: inline-block;
+				margin-right: 15rpx;
+				margin-bottom: 20rpx;
+				padding: 10rpx 20rpx;
+				min-width: 220rpx;
+				line-height: 35rpx;
+				font-size: 26rpx;
+				color: #2D3341;
+				text-align: center;
+				background: #F4F5F7;
+				border: 2rpx solid #F4F5F7;
+				border-radius: 32rpx;
+			}
+		}
+
+		.primary {
+			background: linear-gradient(150deg, #FF6B59 0%, #FF2525 100%) !important;
+			color: #ffffff !important;
+		}
+		.disadleS{
+			background-color:#808080 !important;
+			color: #ffffff !important;
+		}
+	}
+</style>
---
+++ components/sku/index.vue
@@ -0,0 +1,111 @@
+<template>
+	<view class="selectsku">
+		<u-popup v-model="show" mode="bottom" height="90%">
+			<view class="selectBox">
+				<SKUDETAIL v-for="group in specGroups" :key="group.id" :group="group" 
+				@change="changeSelectedSpecs" :selectedSpecs="selectedSpecs" :unSelectSpecs="unSelectSpecs"></SKUDETAIL>
+			</view>
+		</u-popup>
+	</view>
+</template>
+<script>
+	import SKUDETAIL from "./detail.vue"
+	export default {
+		props: {
+			specGroups:{
+				type:Array,
+				default:()=>[]
+			},
+			skus:{
+				type:Object,
+				default:()=>{}
+			}
+		},
+		components:{
+			SKUDETAIL
+		},
+		data() {
+			return {
+				show: false,
+				selectedSpecs:[],
+				
+			}
+		},
+		onLoad() {},
+		watch:{
+		},
+		computed:{
+			// 计算或者筛选不可选的状态
+			unSelectSpecs(){
+				let unArr = []
+				if(this.selectedSpecs.length == 0){
+					return
+				}
+				let selectId = this.selectedSpecs.map(item=>{return item.id})
+				for (let key in this.skusArray) {
+					let arr = this.getSame(this.skusArray[key].specs,selectId)
+					if(arr.length > 0){
+						if(this.skusArray[key].stock_num == 0){
+							unArr.push(this.skusArray[key])
+						}
+					}
+				}
+				let newarr = unArr.map(item=> {return item.specs})
+				return newarr.flatMap(item=>item)
+			},
+			// 计算所有规格组成的数组
+			skusArray(){
+				for (let key in this.skus) {
+					this.skus[key].specs = key.split(';')
+				}
+				return this.skus
+			}
+		},
+		methods: {
+			// 比较数组相同的元素
+			getSame(arr1,arr2){
+				return [...new Set(arr1)].filter(item => 
+				    arr2.includes(item)
+				)
+			},
+			changeShow(item) {
+				let arr = item.split(';') || []
+				if(arr.length > 0){
+					let newArr = []
+					arr.forEach(item=>{
+						newArr.push({
+							id:item
+						})
+					})
+					this.selectedSpecs = newArr
+				}else{
+					this.selectedSpecs = []
+				}
+				
+				this.show = true
+			},
+			// 
+			changeSelectedSpecs(item,group){
+				// item为选中的规格，group为选中的规格所包含的数组
+				const index = this.selectedSpecs.findIndex((i) => group.spec_items.findIndex((b) => b.id === i.id) > -1)
+				 if (index > -1) {
+				    if (item.id === this.selectedSpecs[index].id) {
+				      this.selectedSpecs.splice(index, 1)
+				    } else {
+				      // this.selectedSpecs[index] = item
+					  this.$set(this.selectedSpecs,index,item)
+				    }
+				  } else {
+				    this.selectedSpecs.push(item)
+				  }
+			}
+		}
+	}
+</script>
+<style lang="scss" scoped>
+	.selectsku {
+		.selectBox{
+			padding: 20rpx;
+		}
+	}
+</style>
